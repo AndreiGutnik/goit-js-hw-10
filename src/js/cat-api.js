d1ea32733f1704159ab9fe79753f78b2b72be92c
Andrii Hutnyk <andreigutnik@gmail.com>
2023-07-14 15:30:40 +0200

    First commit

---
+++ src/js/cat-api.js
@@ -0,0 +1,53 @@
+const API_KEY =
+  'live_tS6ZmktikmLWLWcYMPRfLc7GwYln9DrQA1I5NUF7DVjBarEqxo61ydk46u87WtHs';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const END_POINT_BREED = '/breeds';
+const END_POINT_CAT = '/images/search';
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}${END_POINT_BREED}`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data.map(({ id, name }) => ({
+        id: id,
+        name: name,
+      }));
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}${END_POINT_CAT}?breed_ids=${breedId}`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+			if (data && data.length > 0) {
+				return {
+          imageUrl: data[0].url,
+          breedName: data[0].breeds[0].name,
+          description: data[0].breeds[0].description,
+          temperament: data[0].breeds[0].temperament,
+        };
+      } else {
+        throw new Error('Information about cat for this breed is not found!');
+      }
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/js/index.js
@@ -0,0 +1,91 @@
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import catInfoCardTpl from '../templates/cat-info-card.hbs';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const refs = {
+  selectEl: document.querySelector('.breed-select'),
+  loader: document.querySelector('.load'),
+  loaderEl: document.querySelector('.loader'),
+  errorRef: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
+const { selectEl, loader, loaderEl, errorRef, catInfo } = refs;
+
+loaderEl.style.display = 'block';
+loader.style.display = 'block';
+errorRef.style.display = 'none';
+selectEl.style.display = 'none';
+
+// new SlimSelect({
+//   select: '.breed-select',
+//   showContent: 'down',
+//   placeholder: 'Select a breed',
+//   allowDeselect: true,
+//   deselectLabel: '<span class="placeholder-text">Select a breed</span>',
+//   searchable: false,
+// });
+
+setTimeout(() => {
+	fetchBreeds()
+		.then(breeds => {
+			// Create and add breeds - <option> in select
+			markupBreeds(breeds);
+		})
+		.catch(error => {
+			console.error(error);
+			Notiflix.Notify.failure('❌ The breeds of cats are not found!');
+			loader.style.display = 'none';
+			errorRef.style.display = 'block';
+		});
+}, 2000);
+
+selectEl.addEventListener('change', onSelected);
+
+function onSelected() {
+	catInfo.style.display = 'none';
+	loaderEl.style.display = 'block';
+  loader.style.display = 'block';
+
+  const breedId = selectEl.value;
+	setTimeout(() => {
+		fetchCatByBreed(breedId)
+			.then(data => {
+				markupCatInfo(data);
+			})
+			.catch(error => {
+				console.error(error);
+				Notiflix.Notify.failure(
+					'❌ Information about cat for this breed is not found!'
+				);
+				loaderEl.style.display = 'none';
+				loader.style.display = 'none';
+				errorRef.style.display = 'block';
+			});
+	}, 2000);
+}
+
+function markupBreeds(breeds) {
+	const markup = breeds.reduce((acc, { id, name }) => {
+		return acc + `<option value = ${id}>${name}</option>`
+	}, '');
+	selectEl.innerHTML = markup;
+
+  new SlimSelect({
+    select: '.breed-select',
+  });
+
+  selectEl.style.display = 'block';
+  loaderEl.style.display = 'none';
+  loader.style.display = 'none';
+}
+
+function markupCatInfo(data) { 
+	console.log(catInfoCardTpl(data));
+	catInfo.innerHTML = catInfoCardTpl(data);
+
+	loaderEl.style.display = 'none';
+  loader.style.display = 'none';
+  catInfo.style.display = 'flex';
+}
